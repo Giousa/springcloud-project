9801dd14f82c98511fe0a0aa1a1b209a17a31c09
Giousa <65489469@qq.com>
2021-01-24 23:27:01 +0800

    Eureka服务注册中心

---
+++ cloud-eureka-server-7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description:
+ * Author:zhangmengmeng
+ * Date:2021/1/24
+ * Email:65489469@qq.com
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
