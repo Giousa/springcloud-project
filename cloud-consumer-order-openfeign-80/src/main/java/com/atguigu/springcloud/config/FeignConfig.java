1d227116e15d0334ca2efb683c8fac30b49647af
Giousa <65489469@qq.com>
2021-01-27 16:02:47 +0800

    Feign日志

--- cloud-consumer-order-openfeign-80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
+++ cloud-consumer-order-openfeign-80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -1,10 +1,21 @@
 package com.atguigu.springcloud.config;
 
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Description:
  * Author:zhangmengmeng
  * Date:2021/1/27
  * Email:65489469@qq.com
  */
+@Configuration
 public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+
+        return Logger.Level.FULL;
+    }
 }
