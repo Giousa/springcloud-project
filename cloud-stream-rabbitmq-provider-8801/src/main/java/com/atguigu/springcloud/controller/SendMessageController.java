c26a9684df8a283d17c743e4a0f6af38cdefcfa8
Giousa <65489469@qq.com>
2021-01-28 23:09:27 +0800

    Stream发送消息

---
+++ cloud-stream-rabbitmq-provider-8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: SendMessageController
+ * @description:
+ * @author: liusCoding
+ * @create: 2020-06-10 12:37
+ */
+
+@RestController
+public class SendMessageController {
+
+    @Autowired
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider-8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.UUID;
+
+/**
+ * @className: MessageProviderImpl
+ * @description:
+ * @author: zmm
+ * @create: 2020-06-10 12:33
+ */
+
+//定义消息推送
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+
+    @Autowired
+    private MessageChannel output;//消息发送管道
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("****************serial:"+serial);
+        return serial;
+    }
+}
